43c63ee36be0aa2671cc45b2151f7f98e64029e8
minkhantkyaw <minkhantkyaw@my.jcu.edu.au>
2025-06-18 11:39:52 +0800

    django files

--- gym_app/members/urls.py
+++ gym_app/members/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('', views.register_view),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
--- gym_app/members/views.py
+++ gym_app/members/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def members(request):
-  template = loader.get_template('first.html')
+def register_view(request):
+  template = loader.get_template('register.html')
+  return HttpResponse(template.render())
+
+def login_view(request):
+  template = loader.get_template('login.html')
   return HttpResponse(template.render())
