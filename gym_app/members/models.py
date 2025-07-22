916a98b65bc23a3489eeab6541298a28a79e07ce
minkhantkyaw <minkhantkyaw@my.jcu.edu.au>
2025-07-22 20:42:12 +0800

    changes to html page

--- gym_app/members/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  email = models.CharField(max_length=255)
-  student_id = models.IntegerField(null=True)
-  password = models.CharField(max_length=128)
