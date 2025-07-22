11a21e03410a05a3ebf70073e270656285c7cc2d
minkhantkyaw <minkhantkyaw@my.jcu.edu.au>
2025-07-22 21:46:26 +0800

    form is now working

--- JCU_Gym/models.py
+++ JCU_Gym/models.py
@@ -5,4 +5,4 @@ class Member(models.Model):
     First_Name = models.CharField(max_length=255)
     Last_Name = models.CharField(max_length=255)
     Email = models.EmailField(max_length=255)
-    Phone = models.BigIntegerField()
+    Phone = models.CharField(max_length=20)
